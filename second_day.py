4b4e32eb37bdc5b613880b0722b8808dff32318f
thispyt <paassaas21@gmail.com>
2023-12-01 17:51:20 +0100

    begin second day

---
+++ second_day.py
@@ -0,0 +1,10 @@
+#Data types,String,index for printing position char [subscript]
+print("hello"[2])#display char l
+print("1234"[0])#display first number or char 1
+#concat number after add
+print("123"+"456")
+print(123+456)#integer addition
+#float ,bool
+print(3.35)
+print(True)
+print(False)
