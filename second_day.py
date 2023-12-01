4d68373bb0dba3d4167f7fd8d39f1dca122b6f0c
thispyt <paassaas21@gmail.com>
2023-12-01 18:22:49 +0100

    Type Error ,Checking ,Conversion

--- second_day.py
+++ second_day.py
@@ -8,5 +8,21 @@ print(123+456)#integer addition
 print(3.35)
 print(True)
 print(False)
-#len function for string not integer for below display 5
+#len function for string not integer (TypeError int has no len) for below display 5
 print(len("hello"))
+#Type checking and conversation
+# num_char=len(input("what's your name"))
+# print("your name has "+ num_char+" character.")
+#TypeError: can only concatenate str (not "int") to str 
+# solution is parse(conversation [integer to string ]), follow in below 
+#type value in num_char is integer number of character
+num_char=len(input("what's your name \n"))
+new_num_char=str(num_char)
+#a integer number between two string  Error: can only concatenate str (not "int") 
+print("your name has "+ new_num_char+" character.")
+#but print("your name has "+ new_num_char) not
+
+a=float(123)#conversing int to float like 123.0
+print(type(a))#determine type of variable
+print(70+a)#display addition resylt float
+print(str(70)+str(100))#just concating two string
