9d4d357add87fe77c172551b0961f81755db8665
thispyt <paassaas21@gmail.com>
2023-12-03 09:50:30 +0100

    Conditional

---
+++ third_day/conditional.py
@@ -0,0 +1,18 @@
+# if condition:
+#   do this
+# else:
+#   do this
+#restroom
+water_level=50
+if water_level>80:
+    print("drain water")
+else:
+    print("continue")
+#ticket for enjoy in the rollercoaster ?
+# flowshard  draw.io
+print("Welcome to the rollercoaster!")
+height=int(input("what's your height in cm ?"))
+if height>120:
+    print("can ride the rollercoaster, your tall enough ")
+else:
+    print("Sorry, you have to grow taller before you ride the rollercoaster")
