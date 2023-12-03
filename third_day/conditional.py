52bfe189496443493d71df20e110e5c354b9d2f4
thispyt <paassaas21@gmail.com>
2023-12-03 10:01:00 +0100

    Even or Odd

--- third_day/conditional.py
+++ third_day/conditional.py
@@ -16,3 +16,23 @@ if height>120:
     print("can ride the rollercoaster, your tall enough ")
 else:
     print("Sorry, you have to grow taller before you ride the rollercoaster")
+#exercise odd or even
+print("Welcome to the calculator of odd or even")
+number=int(input("Entre a number"))
+if number%2==0:
+    print(f"number {number} is even")
+else :
+    print(f"number {number} is odd")
+#other
+#don't change the below code
+print("Welcome to the calculator of odd or even")
+number_as=int(input("which number do you want to check"))
+#don't change the above code
+#write your code bellow this code
+# number=int(input("Entre a number"))
+if number%2==0:
+    print(f"number {number} is even")
+else :
+    print(f"number {number} is odd")
+#Modulo Operation % > 7%2 -> 7=2+2+2+1 result is 1 
+#6%2=3*'2'+0=0 , 14%4 =3*4(4->2*2)+2=2 ,5/2= 2*2+1=1
